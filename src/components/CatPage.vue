177c8fbd0fa44a7e1b3099cf85462b4749348d6a
judith lourde <judith.lourde@gmail.com>
2022-02-17 21:34:36 +0100

    Add styling

--- src/components/CatPage.vue
+++ src/components/CatPage.vue
@@ -1,11 +1,108 @@
 <template>
-    <section class="home catPage">
-		<header class="home__header catPage__header">
-			<img src="/images/svg/logo.svg" alt="logo-icon">
-		</header>
+    <header class="cat-header">
+        <img src="/images/svg/logo-with-clouds.svg" alt="logo-with-clouds-icon">
+    </header>
 
-        <main >
-            <input type="text" placeholder="Enter your place...">
-        </main>
-    </section>
+    <main class="cat-main">
+        <div class="cat-main__search">
+            <input type="text" placeholder="Enter your place..." v-model="place" @keypress="fetchWeatherData">
+            <button>
+                <img src="/images/svg/search.svg" alt="Search-icon">
+            </button>
+        </div>
+
+        <div class="cat-main__weather">
+            <h2>{{ getPlace }}</h2>
+            <div v-if=" place !== '' " v-bind="convertWholeNumber">{{ weather }}°C</div>
+            <img :src="getWeatherIcon" alt="">
+            <div>{{ getDescription }}</div>
+        </div>
+    </main>
+
+    <div class="cat-image">
+        <img class="" src="/images/svg/cat.svg" alt="cat">
+        <img src="/images/svg/cat-says.svg" alt="Cat-Says-Meow">
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                place: '',
+                getPlace: '',
+                getWeather: '',
+                getDescription: '',
+                getWeatherIcon: '',
+                weather: '',
+            }
+        },
+
+        created() {
+            this.fetchWeatherData();
+        },
+
+        methods: {
+            async fetchWeatherData() {
+                const client_id_key = import.meta.env.VITE_OPENWEATHER_ACCES_KEY;
+                console.log(client_id_key);
+
+                const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${this.place}&units=metric&appid=${client_id_key}`;
+                const responseWeather = await fetch(weatherURL);
+                const weatherOutput = await responseWeather.json();
+
+                console.log(weatherOutput);
+                
+                this.getPlace = weatherOutput.name;
+                this.getWeather = weatherOutput.main.temp;
+                this.getDescription = weatherOutput.weather[0].description;
+
+                this.getWeatherIcon = `https://openweathermap.org/img/wn/${weatherOutput.weather[0].icon}.png`;
+            }
+        },
+
+        computed: {
+            convertWholeNumber() {
+                this.weather = Math.round(this.getWeather);
+                // this.weather = '';
+            }
+        },
+    }
+</script>
+
+<style>
+    .cat-header {
+        height: 20%;
+    }
+
+    .cat-header img {
+        width: 22%;
+    }
+
+    .cat-main {
+        display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+    }
+
+    .cat-main > * {
+        margin-bottom: var(--bottom-small);
+    }
+
+    .cat-image {
+        width: 50%;
+        margin-top: 20px;
+		margin-left: auto;
+		margin-right: auto;
+		display: flex;
+        flex-direction: column;
+		justify-content: center;
+		align-items: center;
+    }
+
+    .cat-image img {
+        width: 30%;
+    }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,39 +1,35 @@
 <template>
-	<section class="home">
-		<header class="home__header">
-			<img src="/images/svg/logo.svg" alt="logo-icon">
-		</header>
+	<header class="home__header">
+		<img src="/images/svg/logo-with-clouds.svg" alt="logo-with-clouds-icon">
+	</header>
 
-		<main class="home__main">
-			<h2>Are you a cat or a dog person?</h2>
+	<main class="home__main">
+		<h2>Are you a cat or a dog person?</h2>
 
-			<h4>(Don't worry we won't judge!)</h4>
+		<h4>(Don't worry we won't judge!)</h4>
 
-			<img src="/images/svg/basket.svg" alt="animal-basket">
-		</main>
+		<img src="/images/svg/basket.svg" alt="animal-basket">
+	</main>
 
-		<div class="home__buttons">
-			<RouterLink :to="{ name: 'cat-page', path: '/cat-page', component: CatPageView }">
-				<div class="home__buttons--fish">
-					<button @click="catButton">
-						<img src="/images/svg/fish.svg" alt="fish-icon">
-						<h4>Cat</h4>
+	<div class="home__buttons">
+		<RouterLink :to="{ name: 'cat-page', path: '/cat-page', component: CatPageView }">
+			<div class="home__buttons--fish">
+				<button @click="catButton">
+					<img src="/images/svg/fish.svg" alt="fish-icon">
+					<h4>Cat</h4>
+				</button>
+			</div>
+		</RouterLink>
+
+		<RouterLink :to="{ name: 'dog-page', path: '/dog-page', component: DogPageView }">
+			<div class="home__buttons-bone">
+					<button>
+						<img src="/images/svg/bone.svg" alt="bone-icon">
+						<h4>Dog</h4>
 					</button>
-				</div>
-			</RouterLink>
-
-			<RouterLink :to="{ name: 'dog-page', path: '/dog-page', component: DogPageView }">
-				<div class="home__buttons-bone">
-						<button>
-							<img src="/images/svg/bone.svg" alt="bone-icon">
-							<h4>Dog</h4>
-						</button>
-				</div>
-			</RouterLink>
-		</div>
-
-	</section>
-	
+			</div>
+		</RouterLink>
+	</div>
 </template>
 
 <script>
@@ -49,38 +45,23 @@
 			}
 			
 		},
-
-		methods: {
-			catButton() {
-
-			},
-
-			dogButton() {
-
-			}
-		}
-
-		
 	}
 </script>
 
 <style>
-	.home {
-		height: 100%;
-		display: grid;
-		grid-template-columns: var(--grid-column-12);
-		grid-template-rows: 100px repeat(2, 1fr);
-		column-gap: 20px;
+	.home__header {
+		height: 20%;
 	}
 
-	.home__header {
-		margin-top: 50px;
-		grid-column: 2/ span 6;
+	.home__header img {
+		width: 22%;
 	}
 
 	.home__main {
-		grid-column: 5/ span 4;
-		grid-row: 2;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 		text-align: center;
 	}
 
@@ -89,10 +70,12 @@
 	}
 
 	.home__buttons {
-		grid-row: 3;
-		grid-column: 5/ span 4;
+		width: 50%;
+		margin-left: auto;
+		margin-right: auto;
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
 	}
 </style>
 
