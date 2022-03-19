3f19377d7431e80b0f8a5042eb9c7104fafbae54
judith lourde <judith.lourde@gmail.com>
2022-03-19 18:20:55 +0100

    Remove localstorage

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -59,13 +59,13 @@
         data() {
             return {  
                 isWeatherVisible: false, 
-                place: localStorage.getItem('place')
+                // place: localStorage.getItem('place')
             }
         },
 
         mounted: async function() { 
             // Calling the fetchGeoCode when the page mounted because it is a async function
-            this.$store.dispatch('fetchGeoCode');
+            // this.$store.dispatch('fetchGeoCode');
         },
 
         created() {
