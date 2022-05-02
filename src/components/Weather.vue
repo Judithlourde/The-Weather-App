0e8622eb8def63097b4f3f5138d14ccc617d51bf
judith lourde <judith.lourde@gmail.com>
2022-05-02 12:59:25 +0200

    Test for netlify

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -65,12 +65,12 @@
 
         mounted() { 
             // Calling the fetchGeoCode when the page mounted because it is a async function
-            // this.$store.dispatch('fetchGeoCode');
+            this.$store.dispatch('fetchGeoCode');
         },
 
         created() {
             // When the app is created it calls the async function fetchGeoCode, it helps when the build server runs
-            this.$store.dispatch('fetchGeoCode');
+            // this.$store.dispatch('fetchGeoCode');
         },
 
         computed: {
