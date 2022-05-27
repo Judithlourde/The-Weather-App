aca295b47a8c8fc38cd95e0a963a68526971eaee
judith lourde <judith.lourde@gmail.com>
2022-05-27 15:33:08 +0200

    Add id for netlify

--- src/modules/weatherLibrary.js
+++ src/modules/weatherLibrary.js
@@ -1,4 +1,4 @@
-const client_id_key = import.meta.env.VITE_OPENWEATHER_ACCES_KEY;
+const client_id_key = '22472f42832eb6d9fd13004d1fb61926';
 const headers = { Accept: "application/json" }
 export default {
     state() {
@@ -120,7 +120,7 @@ export default {
         
         // fetchCurrentLocation fetching data by latitude and longitude
         async fetchCurrentLocation({ state, commit }, position) {               //Destucturing state, commit helps to use many commits and states in the (actions) function 
-            const locationUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${import.meta.env.VITE_OPENWEATHER_ACCES_KEY}`;   
+            const locationUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${'22472f42832eb6d9fd13004d1fb61926'}`;   
             try {
                 const responseLocation = await fetch(locationUrl, headers);
                 const locationOutput = await responseLocation.json();
@@ -146,7 +146,7 @@ export default {
 
         //Destucturing state, commit helps to use many commits and states in the (actions) function 
         async fetchWeatherData({ state, commit }, place) {            
-            const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=${import.meta.env.VITE_OPENWEATHER_ACCES_KEY}`;
+            const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=${'22472f42832eb6d9fd13004d1fb61926'}`;
             try {
                 const responseWeather = await fetch(weatherURL, headers);
                 const weatherOutput = await responseWeather.json();
