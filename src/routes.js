cfe5464d2958ffe7fe8fce6d5228ecad7d036d30
judith lourde <judith.lourde@gmail.com>
2022-05-29 16:42:00 +0200

    Change home component to home view

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-import HomeView from './views/HomeView.vue';
+import Home from './views/Home.vue';
 import Page from './views/Page.vue';
 
 export default [
 	{ name: 'page', path: '/:page_id', component: Page },
-	{ name: 'home', path: '/', component: HomeView },
+	{ name: 'home', path: '/', component: Home },
 ];
